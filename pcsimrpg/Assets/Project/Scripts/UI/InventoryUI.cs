49f2e68511186e9fd010663e6622ba50eb0fa8f8
Gio Yatar <giosef.d.yatar@gmail.com>
2026-03-12 23:00:20 +0800

    Added Inventory UI
    Inventory UI contains the following: Stats Tab, Inventory Tab, Quest Tab, Settings Tab

---
+++ pcsimrpg/Assets/Project/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InventoryUI : MonoBehaviour {
+    public GameObject inventoryPanel;
+
+    public void OpenInventory() {
+        inventoryPanel.SetActive(true);
+    }
+
+    public void CloseInventory() {
+        inventoryPanel.SetActive(false);
+    }
+}
